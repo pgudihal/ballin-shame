239cad72f220dd5ea1164ff55451ae1ad68b23d4
prateekg <pratik.gudihal@gmail.com>
2015-04-19 19:26:31 -0700

    Testing Commit

--- app/src/main/java/com/example/test/picturetest/MainActivity.java
+++ app/src/main/java/com/example/test/picturetest/MainActivity.java
@@ -46,7 +46,7 @@ public class MainActivity extends ActionBarActivity
                 R.id.navigation_drawer,
                 (DrawerLayout) findViewById(R.id.drawer_layout));
     }
-
+//test
     @Override
     public void onNavigationDrawerItemSelected(int position) {
         // update the main content by replacing fragments
